6d00ea86038287c19db2bb1b0f42ca5f4fda59a2
Sai-Dithvika <saidithvika@gmail.com~>
2024-12-16 22:37:29 +0530

    config updated

--- backend/config/dbconfig.js
+++ backend/config/dbconfig.js
@@ -1,24 +1,22 @@
-import dotenv from 'dotenv';
 import pg from 'pg';
+import dotenv from 'dotenv';
 
 dotenv.config();
 const { Pool } = pg;
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
-  idleTimeoutMillis: 20000,
-  max: 12,
-});
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // Important for cloud providers like Supabase
+  }
+});
+console.log(process.env.DATABASE_URL);
 
 const connectDb = async () => {
   try {
     const client = await pool.connect();
     console.log('Database connection established successfully!');
-    client.release(); 
+    client.release();  // Release the client when done
   } catch (err) {
     console.error('Database connection error:', err.message);
   }
